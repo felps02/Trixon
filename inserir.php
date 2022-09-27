1d0a5fb3979eba3d8312a2b9fb459266763586fd
Viinipereira <vinicius_krischanski@estudante.sc.senai.br>
2022-09-27 09:35:48 -0300

    phps certin

---
+++ inserir.php
@@ -0,0 +1,24 @@
+<?php
+    include("conecta.php");
+
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $usuario = $_POST["usuario"];
+    $senha = $_POST["senha"];
+    $data1 = $_POST["data1"];
+    echo ($data1);
+    $cpf = $_POST["cpf"];
+    $telefone = $_POST["telefone"];
+    
+    $comando = $pdo -> prepare("INSERT INTO cadastro_usuario(cpf,nome,email,usuario,senha,data1,telefone) VALUES(:cpf,:nome,:email,:usuario,:senha,:data1,:telefone)");
+    $comando->bindValue(":nome", $nome);
+    $comando->bindValue(":email", $email);
+    $comando->bindValue(":usuario", $usuario);
+    $comando->bindValue(":senha", $senha);
+    $comando->bindValue(":data1", $data1);
+    $comando->bindValue(":cpf", $cpf);
+    $comando->bindValue(":telefone", $telefone);
+    $comando->execute();
+
+    echo("Você foi registrado");
+?>
