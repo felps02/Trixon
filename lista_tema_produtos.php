be697c1b94fcd0147a2af76fb2269049e2f69fc2
Viinipereira <vinicius_krischanski@estudante.sc.senai.br>
2022-11-09 12:22:28 -0300

    aa

--- lista_tema_produtos.php
+++ lista_tema_produtos.php
@@ -30,7 +30,7 @@
     if($comando->rowCount() >= 1)
     {
         //o fetch() transforma o retorno em uma matriz (Use quando você para um registro ou mais, ou seja, uma ou múltiplas linhas da tabela).
-        $lista_temaF = $comando->fetchAll();
+        $lista_temaDI = $comando->fetchAll();
     }
     unset($comando);
     unset($pdo);
