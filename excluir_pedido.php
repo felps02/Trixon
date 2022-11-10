5084945134157522e5b415cc2585ecdb13025fb5
Viinipereira <vinicius_krischanski@estudante.sc.senai.br>
2022-11-10 09:20:03 -0300

    ARRUMO

--- excluir_pedido.php
+++ excluir_pedido.php
@@ -1,34 +1,21 @@
 <?php
-include("conecta.php");
+    include("conecta.php");
 
-$nome_produto = $_POST["nome_produto"];
-$preco_produto = $_POST["preco_produto"];
-$tema_produto = $_POST["tema_produto"];
-$desc_produto =  $_POST["desc_produto"];
-$id_produto = $_POST["id_produto"];
-// $imagem = $_FILES['imagem_perfil'];
-// $extensao = $imagem['type'];
-// $conteudo = file_get_contents($imagem['tmp_name']);
-// $base64 = "data:".$extensao.";base64,".base64_encode($conteudo); //gravar no banco de dados
-// https://rafaelcouto.com.br/salvar-imagem-no-banco-de-dados-com-php-mysql/
+    $id = $_POST['id_produto'];
+    
+    //comando sql.
+    $comando = $pdo->prepare("DELETE FROM cadastro_produto WHERE id_produto = :id_produto;");
 
-// Transformando foto em dados (binário)
-// $conteudo = file_get_contents($arquivo['tmp_name']);
+    //insere valores das variaveis no comando sql.
+    $comando->bindValue(':id_produto',$id);
+    
+    //executa a consulta no banco de dados.
+    $comando->execute();
 
+    //redireciona para a pagina informada.
+    header("location:alterar_pedidos.html");
 
-
-
-$comando=$pdo->prepare("DELETE cadastro_produto WHERE id_produto=id_produto");
-$comando->bindValue(":nome_produto",$nome_produto);
-$comando->bindValue(":id_produto",$id_produto);
-
-
-// $comando->bindValue(":conteudo",$base64);
-
-$comando->execute();
-
-unset($comando);
-unset($pdo);
-
-
-?> 
+    //Fecha declaração e conexão.
+    unset($comando);
+    unset($pdo);
+?>
